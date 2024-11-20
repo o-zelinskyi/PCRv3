0ad116d6e2226a02bca02ab1493a22f4baa34346
Gandalf <korol.dsnylo.g@gmail.com>
2024-11-20 14:57:54 +0200

    Add personal information

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/avatar-Danylo.png"} alt="Korol Danylo" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Korol Danylo</h2>
+          <p style={styles.position}>Customer consultant</p>
         </div>
       </div>
     </main>
